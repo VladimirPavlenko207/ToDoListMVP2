345180f0999042ca47436d478793bdc4c42733a3
VladimirPavlenko207 <paradize73v@gmail.com>
2021-10-25 15:01:37 +0300

    CMTL-8 Создание интерфейса IManager и базового класса BaseManager

---
+++ ToDoList.BL/Managers/BaseManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.BL.Managers
+{
+    public class BaseManager
+    {
+        protected bool GetUpdateResult(RestSharp.IRestResponse response)
+        {
+            return response.StatusCode != 0 && response.StatusCode != HttpStatusCode.BadRequest;
+        }
+    }
+}
---
+++ ToDoList.BL/Managers/IManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.BL.Managers
+{
+    public interface IManager
+    {
+        List<T> Load<T>(string url, List<T> list);
+        bool Add<T>(string url, T obj);
+        bool Edit<T>(string url, T obj);
+        bool Remove(string url, int id);
+    }
+}
