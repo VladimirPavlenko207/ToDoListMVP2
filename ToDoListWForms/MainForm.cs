db935f89ace2afe0842aa98f930049782e884fa1
VladimirPavlenko207 <paradize73v@gmail.com>
2021-10-25 13:40:36 +0300

    Добавьте файлы проекта.

---
+++ ToDoListWForms/MainForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoListWForms
+{
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        #region Categories
+
+        private void comboBoxCategorys_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBoxCategory_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+
+        private void buttonAddCategory_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void buttonRemoveCategory_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void buttonEditCategory_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        #endregion
+
+        #region Tasks
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        #endregion
+
+        #region Current task
+        private void textBoxRightTask_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxRightCat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxRightPrior_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxRightState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePickerRightDeadline_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRightAddTag_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void buttonRightRemoveTag_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void comboBoxRightTags_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listBoxRightTags_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void buttonSaveTask_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void buttonRemoveTask_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void radioButtonsTask_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+        #endregion
+
+        #region Tags
+        private void comboBoxTags_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void buttonAddTag_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void buttonRemoveTag_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void buttonEditTag_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBoxTag_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+        #endregion
+
+        private void buttonUpDate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
